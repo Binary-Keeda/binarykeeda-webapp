cb6af8a9e1bad1076a9f9aabd914bdad20ad554a
Aryan Bhandari <aryanbhandari4077@gmail.com>
2025-03-20 21:11:40 +0530

    Sidebar Added

---
+++ dist/assets/Fullscreen-CdlDIh9c.js
@@ -0,0 +1,1 @@
+import{j as h}from"./index-CmLj0vSx.js";import{c as r}from"./IconButton-DwOx70A_.js";const o=r(h.jsx("path",{d:"M7 14H5v5h5v-2H7zm-2-4h2V7h3V5H5zm12 7h-3v2h5v-5h-2zM14 5v2h3v3h2V5z"}),"Fullscreen");export{o as F};
---
+++ dist/assets/colors-C9LGk773.js
@@ -0,0 +1,1 @@
+const s="#1d1e20",a="#313234";export{s as D,a};
