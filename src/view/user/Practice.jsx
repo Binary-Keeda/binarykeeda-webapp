3313f28decb3860f53ebeeb5dbeb336634006842
Niharika <niharika.202310@gmail.com>
2025-05-23 20:54:21 +0530

    practice card

--- src/view/user/Practice.jsx
+++ src/view/user/Practice.jsx
@@ -1,55 +1,55 @@
-import React, { Suspense, useEffect } from 'react'
-import UserDashboard from './Userdashboard'
-import { Button } from '@mui/material'
-import { Link } from 'react-router-dom'
-const Table = React.lazy(() => import('./components/Table'))
-function PracticePage () {
+import React, { Suspense, useEffect } from "react";
+import UserDashboard from "./Userdashboard";
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
+const Table = React.lazy(() => import("./components/Table"));
+function PracticePage() {
   const CARDS = [
     {
-      head: 'Aptitude',
+      head: "Aptitude",
       description:
-        'Sharpen your problem-solving skills with logical reasoning, quantitative aptitude, and verbal ability.',
-      src: '',
-      link: '/user/practice/Aptitude'
+        "Sharpen your problem-solving skills with logical reasoning, quantitative aptitude, and verbal ability.",
+      src: "",
+      link: "/user/practice/Aptitude",
     },
     {
-      head: 'Miscellaneous',
+      head: "Miscellaneous",
       description:
-        'Explore a variety of topics, including general knowledge, coding challenges, and brain teasers.',
-      src: '',
-      link: '/user/practice/Miscellaneous'
+        "Explore a variety of topics, including general knowledge, coding challenges, and brain teasers.",
+      src: "",
+      link: "/user/practice/Miscellaneous",
     },
     {
-      head: 'Core Subjects',
+      head: "Core Subjects",
       description:
-        'Dive deep into essential subjects like Data Structures, Algorithms, OS, DBMS, and Computer Networks.',
-      src: '',
-      link: '/user/practice/Core'
-    }
-  ]
+        "Dive deep into essential subjects like Data Structures, Algorithms, OS, DBMS, and Computer Networks.",
+      src: "",
+      link: "/user/practice/Core",
+    },
+  ];
   return (
     <Suspense fallback={<>LOADING ... </>}>
       {/* <Table /> */}
-      <section className='grid p-4 md:grid-cols-2  grid-cols-1 lg:grid-cols-4 gap-4'>
+      <section className="grid p-4 md:grid-cols-2  grid-cols-1 lg:grid-cols-4 gap-4">
         {CARDS.map((i, idx) => (
           <div
-            className='flex shadow-lg pb-4 flex-col hover:shadow-xl transition-all duration-200 hover:scale-105 h-[350px] rounded-[22px]'
+            className="flex shadow-lg pb-4 flex-col hover:shadow-xl transition-all duration-200 hover:scale-105 h-[350px] rounded-[22px]"
             key={idx}
           >
             <img
-              className='h-[200px] object-cover rounded-t-[22px]'
+              className="h-[200px] object-cover rounded-t-[22px]"
               src={
-                'https://30dc.graphy.com/s/store/courses/64ff7f1be4b0607f6f9001f6/cover.jpg?v=2'
+                "https://30dc.graphy.com/s/store/courses/64ff7f1be4b0607f6f9001f6/cover.jpg?v=2"
               }
               alt={i.head}
             />
-            <div className='p-6 pb-5'>
-              <h1 className='text-gray-900'>{i.head}</h1>
-              <p className='text-xs text-gray-600 mt-1 mb-2'>{i.description}</p>
+            <div className="p-6 pb-5">
+              <h1 className="text-gray-900">{i.head}</h1>
+              <p className="text-xs text-gray-600 mt-1 mb-2">{i.description}</p>
               <a href={i.link}>
                 <Button
-                  variant='contained'
-                  color='primary'
+                  variant="contained"
+                  color="primary"
                   sx={{ fontSize: 8 }}
                 >
                   Solve
@@ -61,6 +61,6 @@ function PracticePage () {
         ))}
       </section>
     </Suspense>
-  )
+  );
 }
-export default PracticePage
+export default PracticePage;
